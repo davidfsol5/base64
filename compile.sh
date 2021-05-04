dd4fd8b65fd1df9679811726d813e669cc794aa5
David Sisson <davidfsol5@gmail.com>
2021-05-03 20:58:13 -0400

    Cleaned up some failed experiments

--- compile.sh
+++ compile.sh
@@ -6,4 +6,4 @@ em++ -O2 -I $EMSCRIPTEN/system/include -s DISABLE_EXCEPTION_CATCHING=0 -c -std=c
 em++ -s DISABLE_EXCEPTION_CATCHING=0 -o decode-file.js base64.o decode-file.o
 
 # Add Truebit runtime
-node ~/emscripten-module-wrapper/prepare.js decode-file.js --file input.data --file output.data  --run --debug --out=bin --memory-size=25 --upload-ipfs
+node ~/emscripten-module-wrapper/prepare.js decode-file.js --file input.data --file output.data  --run --out=bin --memory-size=25 --upload-ipfs
